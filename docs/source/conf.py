5531ea4eb26d4f23df6b03aa802e3eb7888770cb
bhomnath <bhomnath@salyani.com.np>
2017-05-03 15:55:15 +0545

    updated

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -11,6 +11,11 @@
 #
 # All configuration values have a default; values that are commented out
 # serve to show the default.
+import sphinx_rtd_theme
+
+html_theme = "sphinx_rtd_theme"
+
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 import sys
 import os
@@ -121,7 +126,11 @@ html_theme = 'alabaster'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+html_theme_options = {
+    'collapse_navigation': False,
+    'display_version': False,
+    'navigation_depth': 3,
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
